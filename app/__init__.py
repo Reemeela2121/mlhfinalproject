2f84ee57139b121f9ba6b99c562db6efbe94eaaf
IceMints <IceMints@github.com>
2021-08-05 13:06:10 -0700

    add chat room

--- app/__init__.py
+++ app/__init__.py
@@ -1,51 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_socketio import SocketIO
 
-
-app = Flask(__name__)
-app.config["SECRET_KEY"] = "In development"
-socketio = SocketIO(app)
+socketio = SocketIO()
 
 
-@app.route("/")
-def index():
-    return render_template(
-        "index.html", title="BLOBBER", url="localhost:5000"
-    )  # OR WE COULD CALL IT BLOBBER
+def create_app(debug=False):
+    """Create an application."""
+    app = Flask(__name__)
+    app.debug = debug
+    app.config['SECRET_KEY'] = 'in development'
 
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
-# BLOB ABOUT ANYTHING WITH NEW FRIENDS
-@app.route("/login")  # methods=('GET', 'POST')
-def login():
-    return render_template()
-
-
-# not sure if I want loading as a whole render template and rather as just a transition to next page
-@app.route("/loading")
-def loading_screen():
-    return render_template()
-
-
-@app.route("/create_profile")
-def create_profile():
-    return render_template()
-
-
-@app.route("/profile")
-def profile():
-    return render_template()
-
-
-@app.route("/quiz")
-def questionnaire():
-    return render_template()
-
-
-@app.route("/dashboard")
-def chat_dashboard():
-    return render_template()
-
-
-@app.route("/open_chat")
-def open_chat():
-    return render_template()
+    socketio.init_app(app)
+    return app

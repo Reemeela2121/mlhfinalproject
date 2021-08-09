e6a7c4019479a522e848b30fdf5d55c0244ec965
joying-yang <joying0614@gmail.com>
2021-08-09 16:42:20 -0700

    fix merge conflict

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,24 @@
-from flask import Flask, render_template, session, escape, request, redirect, url_for
-from flask_migrate import Migrate
+
+from flask import Flask, render_template, request, redirect, url_for, session, abort
+from flask_socketio import SocketIO, join_room, leave_room, emit
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+
 from dotenv import load_dotenv, find_dotenv
-import os
+import os, requests
+
+# load environmental variables
+load_dotenv(find_dotenv())
+
 
 app = Flask(__name__)
+app.debug = True
+app.config["SECRET_KEY"] = "in development"
 
-load_dotenv(find_dotenv())
+
+# initializing Socket IO
+socketio = SocketIO(app, manage_session=False)
 
 # add database
 app.config[
@@ -20,12 +30,11 @@ app.config[
     port=5432,
     table=os.getenv("POSTGRES_DB"),
 )
+
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.secret_key = "secret-key"
-app.config["SECRET_KEY"] = "mysecret"
 
 # initialize the database
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 migrate = Migrate(app, db)
 
 # User Model
@@ -77,15 +86,83 @@ def testing():
         return "you r forever alone"
     return "Your best match is with user: " + User.query.filter_by(id=highest_match_id).first().username
 
+# Google reCaptcha sitekey
+# site_key = os.getenv("SITE_KEY")
+
+# reCaptcha verification
+# def is_human(captcha_response):
+#     secret = os.getenv("SECRET_KEY")
+#     payload = {"response": captcha_response, "secret": secret}
+#     response = requests.post(
+#         "https://www.google.com/recaptcha/api/siteverify", data=payload
+#     )
+#     response_text = response.json()
+#     return response_text["success"]
+
+# matching algorithm
+def match(own_hobbies, current_user_hobbies):
+    own_hobbies_arr = own_hobbies.split(", ")
+    current_user_hobbies_arr = current_user_hobbies.split(", ")
+    common = list(set(own_hobbies_arr).intersection(current_user_hobbies_arr))
+    return len(common)
+
+
+@app.route("/testing", methods=["GET", "POST"])
+def testing():
+    if "username" in session:
+        current_user = session["username"]
+        print(current_user)
+        print(User.query.filter_by(username=current_user).first().hobbies)
+    else:
+        return "u r not logged in"
+    own_hobbies = User.query.filter_by(username=current_user).first().hobbies
+    rows = User.query.count()
+    highest_match_value = -1
+    highest_match_id = None
+    for i in range(2, rows + 2):
+        if User.query.filter_by(id=i).first().username == current_user:
+            continue
+        current_user_hobbies = User.query.filter_by(id=i).first().hobbies
+        match_value = match(own_hobbies, current_user_hobbies)
+        if match_value > highest_match_value and match_value > 0:
+            highest_match_value = highest_match_value
+            highest_match_id = i
+        print(current_user_hobbies)
+    if highest_match_id == None:
+        return "you r forever alone"
+    return (
+        "Your best match is with user: "
+        + User.query.filter_by(id=highest_match_id).first().username
+    )
+
+
+# Home page
 @app.route("/")
 def index():
-    if "username" in session:
-        username = session["username"]
+    return render_template("index.html", title="BLOBBER", url="localhost:5000")
+
+
+# dashboard
+@app.route("/dashboard", methods=["GET", "POST"])
+def dashboard():
+    return render_template("dashboard.html")
+
+
+# chat-room
+@app.route("/chat", methods=["GET", "POST"])
+def chat():
+    if request.method == "POST":
+        username = request.form["username"]
+        room = request.form["room"]
+        # Store the data in session
+        session["username"] = username
+        session["room"] = room
+        return render_template("chat.html", session=session)
     else:
-        username = ""
-    return render_template(
-        "index.html", title="BLOBBER", url="localhost:5000", username=username
-    )  # OR WE COULD CALL IT BLOBBER
+        if session.get("username") is not None:
+            return render_template("chat.html", session=session)
+        else:
+            return redirect(url_for("index"))
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -108,8 +185,8 @@ def register():
             new_user = User(username, generate_password_hash(password), hobbies)
             db.session.add(new_user)
             db.session.commit()
-            session["username"] = username
-            return redirect(url_for("index"))
+            # session["username"] = username
+            return redirect(url_for("login"))
         else:
             return render_template("register.html", error=error)
 
@@ -144,36 +221,70 @@ def logout():
     return redirect(url_for("index"))
 
 
-# not sure if I want loading as a whole render template and rather as just a transition to next page
-@app.route("/loading")
-def loading_screen():
-    return render_template()
+@app.route("/faq")
+def faq():
+    return render_template("faq.html", title="FAQ", url="faq")
 
 
-@app.route("/create_profile")
-def create_profile():
-    return render_template()
-
-
-@app.route("/profile")
-def profile():
-    return render_template()
+@app.route("/about")
+def about():
+    return render_template("about.html", title="about", url="about")
 
 
 @app.route("/quiz")
 def questionnaire():
-    return render_template()
+    return render_template("questions.html", title="questionnaire", url="quiz")
 
 
-@app.route("/dashboard")
-def chat_dashboard():
-    return render_template()
+# @app.route("/loading")
+# def loading_screen():
+#     return render_template()
 
 
-@app.route("/open_chat")
-def open_chat():
-    return render_template()
+# @app.route("/create_profile")
+# def create_profile():
+#     return render_template()
+
+
+# @app.route("/profile")
+# def profile():
+#     return render_template()
+
+
+# SocketIO events
+@socketio.on("join", namespace="/chat")
+def join(message):
+    """Sent by clients when they enter a room.
+    A status message is broadcast to all people in the room."""
+    room = session.get("room")
+    join_room(room)
+    emit(
+        "status", {"msg": session.get("username") + " has entered the room."}, room=room
+    )
+
+
+
+@socketio.on("text", namespace="/chat")
+def text(message):
+    """Sent by a client when the user entered a new message.
+    The message is sent to all people in the room."""
+    room = session.get("room")
+    emit(
+        "message", {"msg": session.get("username") + " : " + message["msg"]}, room=room
+    )
+
+
+
+@socketio.on("left", namespace="/chat")
+def left(message):
+    """Sent by clients when they leave a room.
+    A status message is broadcast to all people in the room."""
+    room = session.get("room")
+    username = session.get("username")
+    leave_room(room)
+    session.clear()
+    emit("status", {"msg": username + " has left the room."}, room=room)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app)

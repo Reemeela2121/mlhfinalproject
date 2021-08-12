c8917dfe69374fcd0fbc26657e99c4c37597e715
IceMints <IceMints@github.com>
2021-08-12 19:37:14 +0000

    add joying's database schema

--- app/__init__.py
+++ app/__init__.py
@@ -43,15 +43,50 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(200), nullable=False, unique=True)
     password = db.Column(db.String(), nullable=False)
-    hobbies = db.Column(db.String(), nullable=False)
+    pronouns = db.Column(db.String(), nullable=True)
+
+    age = db.Column(db.Integer, nullable=True)
+    gender = db.Column(db.String(), nullable=True)
+    sexuality = db.Column(db.String(), nullable=True)
+    personality = db.Column(db.String(), nullable=True)
+    horoscope = db.Column(db.String(), nullable=True)
+    hobbies = db.Column(db.String(), nullable=True)
+    term = db.Column(db.String(), nullable=True)
+    profession = db.Column(db.String(), nullable=True)
+    music = db.Column(db.String(), nullable=True)
 
     def __init__(self, username, password, hobbies):
         self.username = username
         self.password = password
+        self.pronouns = pronouns
+
+        self.age = age
+        self.gender = gender
+        self.sexuality = sexuality
+        self.personality = personality  # introvert / extrovert / ambivert
+        self.horoscope = horoscope
         self.hobbies = hobbies
+        self.term = term  # long term friend or short term friend
+        self.profession = profession
+        self.music = music  # music taste
 
     def __repr__(self):
         return f"<User {self.username}>"
+
+
+# Room Model
+class Room(db.Model):
+    __tablename__ = "rooms"
+    id = db.Column(db.Integer, primary_key=True)
+    room_name = db.Column(db.String())
+    occupancy = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, room_name, occupancy):
+        self.room_name = room_name
+        self.occupancy = occupancy
+
+    def __repr__(self):
+        return f"{self.id},{self.room_name}"
 
 
 # Google reCaptcha sitekey
@@ -73,6 +108,74 @@ def match(own_hobbies, current_user_hobbies):
     current_user_hobbies_arr = current_user_hobbies.split(", ")
     common = list(set(own_hobbies_arr).intersection(current_user_hobbies_arr))
     return len(common)
+
+
+def age_score(own_age, other_age):
+    diff = abs(own_age - other_age)
+    if diff == 0:
+        diff = 1
+    return 5 / diff
+
+
+def gender_score(own_gender, other_gender):
+    if own_gender == other_gender:
+        return 3
+    else:
+        return 1
+
+
+def personality_score(own_personality, other_personality):
+    if own_personality == other_personality:
+        return 3
+    elif own_personality == "ambivert" or other_personality == "ambivert":
+        return 1.5
+    else:
+        return 0
+
+
+def horoscope_score(
+    own_horoscope, other_horoscope
+):  # i dont believe in horoscopes but this is kinda fun
+    if (own_horoscope == "cancer" or other_horoscope == "cancer") and (
+        own_horoscope == "saggitarius" or other_horoscope == "saggitarius"
+    ):
+        return 3
+    elif (own_horoscope == "taurus" or other_horoscope == "taurus") and (
+        own_horoscope == "pisces" or other_horoscope == "pisces"
+    ):
+        return 3
+    elif (own_horoscope == "gemini" or other_horoscope == "gemini") and (
+        own_horoscope == "capricorn" or other_horoscope == "capricorn"
+    ):
+        return 3
+    elif (own_horoscope == "libra" or other_horoscope == "libra") and (
+        own_horoscope == "leo" or other_horoscope == "leo"
+    ):
+        return 3
+    elif (own_horoscope == "scorpio" or other_horoscope == "scorpio") and (
+        own_horoscope == "aries" or other_horoscope == "aries"
+    ):
+        return 3
+    elif (own_horoscope == "virgo" or other_horoscope == "virgo") and (
+        own_horoscope == "aquarius" or other_horoscope == "aquarius"
+    ):
+        return 3
+    else:
+        return 0
+
+
+def hobby_score(own_hobbies, other_hobbies):
+    own_hobbies_arr = own_hobbies.split(", ")
+    other_hobbies_arr = other_hobbies.split(", ")
+    common = list(set(own_hobbies_arr).intersection(other_hobbies_arr))
+    return len(common)
+
+
+def term_score(own_term, other_term):
+    if own_term == other_term:
+        return 4
+    else:
+        return 0
 
 
 @app.route("/testing", methods=["GET", "POST"])

ee9a774b4f367a955bfd398bb68fa9c392660cb6
IceMints <IceMints@github.com>
2021-08-17 00:30:17 -0700

    add more test to prod script

--- prod-test.sh
+++ prod-test.sh
@@ -5,7 +5,7 @@ EXITSTATUS=0
 # website data
 URL="https://blobber.tech"
 PAGES="/ /dashboard /login /register /faq /about"
-METHODS="GET POST DELETE PUT PATCH"
+METHODS="GET"
 
 # Testing endpoints with request methods
 echo ${cyan}Testing endpoints with request methods${reset_color}
@@ -26,6 +26,24 @@ EXITSTATUS=1
 fi
 done
 done
+
+# Function to post endpoints
+testing_endpoint () {
+GETVAR=$1
+if [[ $GETVAR == $2 ]]; then
+echo $GETVAR ${green}'SUCCESS'${reset_color}
+else
+echo $GETVAR ${red}'ERROR'${reset_color}
+EXITSTATUS=2
+fi
+}
+
+echo ${cyan}Posting to /login endpoint with register user and empty password${reset_color}
+testing_endpoint "$(curl -sX POST -d "username=test" -o /dev/null -w '%{http_code}\n' $URL'/login')" "400"
+
+echo Posting to /register endpoint with usr test, pw test pw test
+testing_endpoint "$(curl -sX POST -d "username=test&password=test&password2=test" -o /dev/null -w '%{http_code}\n' $URL'/register')" "200"
+
 
 echo 'EXIT STATUS = '$EXITSTATUS
 exit $EXITSTATUS
